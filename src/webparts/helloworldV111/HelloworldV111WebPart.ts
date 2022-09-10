7b55889d34b8d13b089a696551ad5d922bab9863
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-09-10 01:02:17 -0400

    npm install and added text to component

--- src/webparts/helloworldV111/HelloworldV111WebPart.ts
+++ src/webparts/helloworldV111/HelloworldV111WebPart.ts
@@ -24,6 +24,7 @@ export default class HelloworldV111WebPart extends BaseClientSideWebPart<IHellow
               <span class="${ styles.title }">Welcome to SharePoint!</span>
               <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <p class="${ styles.description }">${escape('Web part has been run through upgrade cli to v1.14')}</p>
               <a href="https://aka.ms/spfx" class="${ styles.button }">
                 <span class="${ styles.label }">Learn more</span>
               </a>
