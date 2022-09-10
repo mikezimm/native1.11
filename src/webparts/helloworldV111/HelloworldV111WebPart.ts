453aceb77b9a5f7fcc2a6eda6d04ade7e1685639
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-09-10 00:14:35 -0400

    Initial build
    verified it gulped

---
+++ src/webparts/helloworldV111/HelloworldV111WebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './HelloworldV111WebPart.module.scss';
+import * as strings from 'HelloworldV111WebPartStrings';
+
+export interface IHelloworldV111WebPartProps {
+  description: string;
+}
+
+export default class HelloworldV111WebPart extends BaseClientSideWebPart<IHelloworldV111WebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.helloworldV111 }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Welcome to SharePoint!</span>
+              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <a href="https://aka.ms/spfx" class="${ styles.button }">
+                <span class="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/helloworldV111/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHelloworldV111WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HelloworldV111WebPartStrings' {
+  const strings: IHelloworldV111WebPartStrings;
+  export = strings;
+}
